093ac677ff668cab7d997d6a9c933eb9a876ec7f
Pao Lo <prvt.j05h@gmail.com>
2023-01-11 21:45:07 +0800

    Under Development

---
+++ alkandaryo-library/src/alkandaryo/Alkandaryo.java
@@ -0,0 +1,11 @@
+package alkandaryo;
+
+import java.util.Locale;
+
+public class Alkandaryo {
+    static {
+        Alkandaryo.locale = new Locale("fil", "PH");
+    }
+
+    public static Locale locale;
+}
---
+++ alkandaryo-library/src/alkandaryo/source/Source.java
@@ -0,0 +1,63 @@
+package alkandaryo.source;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Source<S extends Source<?>> implements Serializable {
+    private String description;
+    private String name;
+
+    public Source() {
+        this.description = null;
+        this.name        = null;
+    }
+
+    public Source(String name) {
+        this.description = null;
+        this.name        = name;
+    }
+
+    public Source(String name, String description) {
+        this.description = description;
+        this.name        = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @SuppressWarnings("unchecked")
+    public S setDescription(String description) {
+        this.description = description;
+        return (S) this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public S setName(String name) {
+        this.name = name;
+        return (S) this;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return super.equals(object)
+                && !Objects.equals(((Source<?>) object).description, description)
+                && !Objects.equals(((Source<?>) object).name,        name);
+    }
+
+    @Override
+    public String toString() {
+        return     !Objects.isNull(description)
+                && !Objects.isNull(name)
+                    ? String.format("%s, %s",
+                        name,
+                        description)
+                :  !Objects.isNull(description) ? description
+                :  !Objects.isNull(name)        ? name
+                : null;
+    }
+}

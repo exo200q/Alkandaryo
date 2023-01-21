88025b303a035008c11f77209efc36629e2c6c67
wizard <prvt.j05h@gmail.com>
2023-01-21 22:33:52 +0800

    Under Development

--- alkandaryo-library/src/Main.java
+++ alkandaryo-library/src/Main.java
@@ -1,8 +1,10 @@
+import alkandaryo.date.Day;
 import alkandaryo.source.Balance;
 
 public class Main {
     public static void main(String[] args) {
-        Balance balance = new Balance(1000);
-        System.out.println(balance.setName("Hello"));
+        Day day = new Day(5);
+
+        System.out.println(day);
     }
 }
---
+++ alkandaryo-library/src/alkandaryo/date/Day.java
@@ -0,0 +1,56 @@
+package alkandaryo.date;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Day extends Date.Builder<Day> {
+   private int month;
+   private int year;
+
+   public Day() {
+      this.month  = localDate.getMonthValue();
+      this.number = localDate.getDayOfMonth();
+      this.year   = localDate.getYear();
+   }
+
+   public Day(int number) {
+      this.month  = localDate.getMonthValue();
+      this.number = number;
+      this.year   = localDate.getYear();
+   }
+
+   public void setMonth(int month) {
+      this.month = month;
+   }
+
+   public int getMonth() {
+      return month;
+   }
+
+   public void setYear(int year) {
+      this.year = year;
+   }
+
+   public int getYear() {
+      return year;
+   }
+
+   public String getWeek() {
+      return Date.baseName(LocalDate
+            .of(year, month, number)
+            .getDayOfWeek()
+            .name());
+   }
+
+   @Override
+   public boolean equals(Object object) {
+      return super.equals(object)
+            && Objects.equals(((Day) object).month, month)
+            && Objects.equals(((Day) object).year,  year);
+   }
+
+   @Override
+   public String toString() {
+      return String.format("%s, %s", getWeek(), number);
+   }
+}

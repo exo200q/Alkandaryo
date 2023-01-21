fb072496019f58422d5aabf2eda7ff92ef46ec2c
wizard <prvt.j05h@gmail.com>
2023-01-21 23:12:08 +0800

    Under Development

--- alkandaryo-library/src/Main.java
+++ alkandaryo-library/src/Main.java
@@ -1,8 +1,5 @@
-import alkandaryo.source.Balance;
-
 public class Main {
     public static void main(String[] args) {
-        Balance balance = new Balance(1000);
-        System.out.println(balance.setName("Hello"));
+
     }
 }
